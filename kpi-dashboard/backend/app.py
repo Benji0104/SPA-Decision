4d1f4199e0aefcae83e44e4fe37fabb1e5270c37
Benji0104 <benjamingildese82@gmail.com>
2024-10-13 15:48:20 -0500

    m

--- kpi-dashboard/backend/app.py
+++ kpi-dashboard/backend/app.py
@@ -8,6 +8,3 @@ CORS(app)
 @app.route('/kpi-data/<compay>', methods=['GET'])
 def get_kpi_data(company):
     return jsonify(kpi_data)
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
